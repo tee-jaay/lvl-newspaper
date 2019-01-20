b79ae8b0c335ee2dc267d1b282ed084aae19ad27
_tam <tamjid84@yahoo.com>
2019-01-20 10:09:01 +0600

    blog - cdn css, js

--- resources/views/frontend/partials/footer-scripts.blade.php
+++ resources/views/frontend/partials/footer-scripts.blade.php
@@ -1,23 +1,31 @@
 <!-- JS
 ============================================ -->
-<!-- jQuery JS -->
 @if (config('app.env') == 'production')
+    <!-- jQuery JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/popper.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.min.js"></script>
+    {{--Toastr js--}}
+    <script src="https://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
 @else
+    <!-- jQuery JS -->
     <script src="{{asset('assets/Frontend/js/vendor/jquery-1.12.0.min.js')}}"></script>
+    <!-- Popper JS -->
+    <script src="{{asset('assets/Frontend/js/popper.min.js')}}"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{asset('assets/Frontend/js/bootstrap.min.js')}}"></script>
+    {{--Toastr js--}}
+    <script src="{{asset('assets/Frontend/js/toastr.min.js')}}"></script>
 @endif
-<!-- Popper JS -->
-<script src="{{asset('assets/Frontend/js/popper.min.js')}}"></script>
-<!-- Bootstrap JS -->
-<script src="{{asset('assets/Frontend/js/bootstrap.min.js')}}"></script>
-<!-- Plugins JS -->
-<script src="{{asset('assets/Frontend/js/plugins.js')}}"></script>
-<!-- rypp JS -->
-<script src="{{asset('assets/Frontend/js/rypp.js')}}"></script>
-<!-- Ajax Mail JS -->
-<script src="{{asset('assets/Frontend/js/ajax-mail.js')}}"></script>
-<!-- Main JS -->
-<script src="{{asset('assets/Frontend/js/main.js')}}"></script>
-{{--Toastr js--}}
-<script src="https://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+    <!-- Plugins JS -->
+    <script src="{{asset('assets/Frontend/js/plugins.js')}}"></script>
+    <!-- rypp JS -->
+    <script src="{{asset('assets/Frontend/js/rypp.js')}}"></script>
+    <!-- Ajax Mail JS -->
+    <script src="{{asset('assets/Frontend/js/ajax-mail.js')}}"></script>
+    <!-- Main JS -->
+    <script src="{{asset('assets/Frontend/js/main.js')}}"></script>
+
 {!! Toastr::message() !!}
--- resources/views/frontend/partials/head-styles.blade.php
+++ resources/views/frontend/partials/head-styles.blade.php
@@ -1,9 +1,18 @@
 <!-- CSS
 ============================================ -->
+@if (config('app.env') == 'production')
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{asset('assets/Frontend/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css">
 <!-- Icon Font CSS -->
-<link rel="stylesheet" href="{{asset('assets/Frontend/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+{{--Toastr css--}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.css">
+@else
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/Frontend/css/bootstrap.min.css')}}">
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="{{asset('assets/Frontend/css/font-awesome.min.css')}}">
+@endif
 <!-- Plugins CSS -->
 <link rel="stylesheet" href="{{asset('assets/Frontend/css/plugins.css')}}">
 <!-- rypp -->
@@ -11,6 +20,6 @@
 <!-- Style CSS -->
 <link rel="stylesheet" href="{{asset('assets/Frontend/css/style.css')}}">
 {{--Toastr--}}
-<link rel="stylesheet" href="https://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+<link rel="stylesheet" href="{{asset('assets/Frontend/css/toastr.css')}}">
 {{--Google Font--}}
 <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
