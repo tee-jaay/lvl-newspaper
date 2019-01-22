5a54844a12569ff50a8715c82d1b8ca79e4e6312
_tam <tamjid84@yahoo.com>
2019-01-22 18:20:30 +0600

    blog - jquery cdn 3.3.1

--- resources/views/frontend/partials/footer-scripts.blade.php
+++ resources/views/frontend/partials/footer-scripts.blade.php
@@ -2,7 +2,8 @@
 ============================================ -->
 @if (config('app.env') == 'production')
     <!-- jQuery JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>--}}
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/popper.min.js"></script>
     <!-- Bootstrap JS -->
