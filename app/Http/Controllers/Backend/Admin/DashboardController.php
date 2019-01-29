a49e34e0672138f33a4c35b7539355092341bcc1
_tam <tamjid84@yahoo.com>
2019-01-29 18:08:20 +0600

    dashboard - blog - infobox

--- app/Http/Controllers/Backend/Admin/DashboardController.php
+++ app/Http/Controllers/Backend/Admin/DashboardController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers\Backend\Admin;
 
+use App\Models\Blog\Category;
+use App\Models\Blog\Comment;
+use App\Models\Blog\Post;
+use App\Models\Blog\Tag;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('backend.dashboard.index');
+        $posts_count = Post::all()->count();
+        $categories_count = Category::all()->count();
+        $tags_count = Tag::all()->count();
+        $comments_count = Comment::all()->count();
+        return view('backend.dashboard.index', compact('posts_count','categories_count','tags_count','comments_count'));
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -8,7 +8,53 @@
 
 @section('content')
     <div class="block-header">
-        <h2>WELCOME TO DASHBOARD</h2>
+        <h3>BLOG</h3>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-red hover-zoom-effect">
+                <div class="icon">
+                    <i class="material-icons">pages</i>
+                </div>
+                <div class="content">
+                    <div class="text">POSTS</div>
+                    <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20">@if (isset($posts_count)) {{$posts_count}} @else N/A @endif</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-indigo hover-zoom-effect">
+                <div class="icon">
+                    <i class="material-icons">category</i>
+                </div>
+                <div class="content">
+                    <div class="text">CATEGORIES</div>
+                    <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20">@if (isset($categories_count)) {{$categories_count}} @else N/A @endif</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-purple hover-zoom-effect">
+                <div class="icon">
+                    <i class="material-icons">bookmark</i>
+                </div>
+                <div class="content">
+                    <div class="text">TAGS</div>
+                    <div class="number count-to" data-from="0" data-to="117" data-speed="1000" data-fresh-interval="20">@if (isset($tags_count)) {{$tags_count}} @else N/A @endif</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-deep-purple hover-zoom-effect">
+                <div class="icon">
+                    <i class="material-icons">comment</i>
+                </div>
+                <div class="content">
+                    <div class="text">COMMENTS</div>
+                    <div class="number count-to" data-from="0" data-to="1432" data-speed="1500" data-fresh-interval="20">@if (isset($comments_count)) {{$comments_count}} @else N/A @endif</div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
