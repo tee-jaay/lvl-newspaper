b54f89c647b1187d23177ab10b34e30b44ed1fd6
_tam <tamjid84@yahoo.com>
2019-01-30 23:24:35 +0600

    dashboard - login - welcome toastrjs

--- app/Http/Controllers/Backend/Admin/DashboardController.php
+++ app/Http/Controllers/Backend/Admin/DashboardController.php
@@ -6,16 +6,22 @@ use App\Models\Blog\Category;
 use App\Models\Blog\Comment;
 use App\Models\Blog\Post;
 use App\Models\Blog\Tag;
+use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $posts_count = Post::all()->count();
         $categories_count = Category::all()->count();
         $tags_count = Tag::all()->count();
         $comments_count = Comment::all()->count();
-        return view('backend.dashboard.index', compact('posts_count','categories_count','tags_count','comments_count'));
+
+        Toastr::success('Welcome to the Dashboard!', 'Hello ' . Auth::user()->name . ' !', ["positionClass" => "toast-top-center"]);
+
+        return view('backend.dashboard.index', compact('posts_count', 'categories_count', 'tags_count', 'comments_count'));
     }
 }
