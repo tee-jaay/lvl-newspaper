4c057440d0c2bb745d67d3a34ca7bb69dfd72655
_tam <tamjid84@yahoo.com>
2019-02-03 23:09:55 +0600

    dashboard - register,reset,email pages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,47 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@push('css')
+@endpush
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <div class="card">
+        <div class="body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form id="forgot_password" method="POST" action="{{ route('password.email') }}">
+                {{ csrf_field() }}
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                <div class="msg">
+                    Enter your email address that you used to register. We'll send you an email with your username and a
+                    link to reset your password.
+                </div>
+                <div class="input-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <span class="input-group-addon">
+                            <i class="material-icons">email</i>
+                        </span>
+                    <div class="form-line">
+                        <input type="email" class="form-control" name="email" placeholder="Email"  value="{{ old('email') }}" required>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <button class="btn btn-block btn-lg bg-pink waves-effect" type="submit">SEND PASSWORD RESET LINK
+                </button>
+
+                <div class="row m-t-20 m-b--5 align-center">
+                    <a href="{{route('login')}}">Sign In!</a>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
+
+@push('js')
+@endpush
