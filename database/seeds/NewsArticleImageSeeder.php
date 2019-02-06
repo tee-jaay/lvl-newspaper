d97c75d6cb9de6c4b6864454da3ed78b734e17e3
_tam <tamjid84@yahoo.com>
2019-02-07 01:18:36 +0600

    News article images seeder

---
+++ database/seeds/NewsArticleImageSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class NewsArticleImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for($i = 1; $i < 143; $i++) {
+            App\Models\News\NewsArticleImages::create([
+                'article_id' => $i,
+                'header' => $faker->imageUrl(770, 294),
+                'right' => $faker->imageUrl(376, 522),
+                'main' => $faker->imageUrl(730, 400),
+                'bottom' => $faker->imageUrl(730, 153)
+            ]);
+        }
+    }
+}
